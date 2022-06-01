3fb2f409c5a765a53d0f38f92c3ed1313492759e
Alfonso Gamboa <gtxent@gmail.com>
2022-06-01 00:59:46 -0700

    move rtmp-stream.sh to PATH /bin

--- SD_ROOT/wz_mini/bin/rtmp-stream.sh
+++ SD_ROOT/wz_mini/bin/rtmp-stream.sh
@@ -1,5 +1,7 @@
 #! /bin/sh
 
+RTMP_LOG=/opt/wz_mini/log/rtmp.log
+
 source /opt/wz_mini/wz_mini.conf
 
 if [[ "$RTSP_PASSWORD" == "" ]]; then
@@ -26,37 +28,45 @@ FACEBOOK_KEY=""
 RTSP_STREAM="video1_unicast"
 
 VIDEO_SOURCE="rtsp://"$RTSP_LOGIN":"$RTSP_PASSWORD"@0.0.0.0:"$RTSP_PORT"/$RTSP_STREAM"
-AUDIO="-c:a libfdk_aac -afterburner 1 -channels 1 -b:a 256k -profile:a aac_he -ar 16000 -strict experimental"
+
+AUDIO_OPTIONS="-c:a libfdk_aac -afterburner 1 -channels 1 -b:a 256k -profile:a aac_he -ar 16000 -strict experimental"
+VIDEO_OPTIONS="-c:v copy -coder 1 -pix_fmt yuv420p -g 30 -bf 0"
 
 if [[ "$2" == "no_audio" ]]; then
-        echo NOAUDIO
+        echo "NO_AUDIO: audio disabled on RTMP Stream."
         AUDIO="-an"
 fi
 
 if [[ "$1" == "youtube" ]]; then
-        echo "youtube"
+        echo "RTMP Streaming to: YouTube"
         STREAM_PROVIDER="$YOUTUBE_URL"
         KEY="$YOUTUBE_KEY"
 elif [[ "$1" == "twitch" ]]; then
-        echo "twitch"
+        echo "RTMP Streaming to: twitch"
         STREAM_PROVIDER="$TWITCH_URL"
         KEY="$TWITCH_KEY"
 elif [[ "$1" == "facebook" ]]; then
-        echo "facebook"
+        echo "RTMP Streaming to: facebook"
         STREAM_PROVIDER="$FACEBOOK_URL"
         KEY="$FACEBOOK_KEY"
 else
-echo "Usage:"
-echo "rtmp facebook"
-echo "rtmp twitch"
-echo "rtmp youtube"
-echo "Update the script with your stream keys first"
-exit 0
+	echo "Usage: rtmp-stream.sh <service> <no_audio>"
+	echo ""
+	echo "Available services:"
+	echo "rtmp-stream.sh facebook"
+	echo "rtmp-stream.sh twitch"
+	echo "rtmp-stream.sh youtube"
+	echo "rtmp-stream.sh <service> no_audio disables audio"
+	echo ""
+	echo "Update the script with your stream keys first."
+	exit 0
 fi
 
 sync;echo 3 > /proc/sys/vm/drop_caches
 
+echo "LOG FILE: $RTMP_LOG"
+
 $FFMPEG_BINARY \
 -rtsp_transport udp -y \
 -i "$VIDEO_SOURCE" \
--c:v copy -coder 1 -pix_fmt yuv420p -g 30 -bf 0 $AUDIO -aspect 16:9 -f flv "$STREAM_PROVIDER/$KEY"
+$VIDEO_OPTIONS $AUDIO_OPTIONS -aspect 16:9 -f flv "$STREAM_PROVIDER/$KEY" > $RTMP_LOG 2>&1 &
