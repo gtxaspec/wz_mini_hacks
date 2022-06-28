71fbcd0707a8b71803f535f55f2c680f416f87f6
Alfonso Gamboa <gtxent@gmail.com>
2022-06-28 14:14:58 -0700

    add pan support for compile_image.sh
    Updated compile_image.sh to support PANv1.  Use: ./compile_image.sh pan

--- v2_install/compile_image.sh
+++ v2_install/compile_image.sh
@@ -2,6 +2,7 @@
 
 UA_STRING="Mozilla/5.0 (Macintosh; Intel Mac OS X 12_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Safari/605.1.15"
 DL_URL=$(wget --header="Accept: text/html" --user-agent="$UA_STRING" -qO- https://support.wyze.com/hc/en-us/articles/360024852172-Release-Notes-Firmware | grep -Eo "(http|https)://[a-zA-Z0-9./?=_-]*" | grep /v2/ |   sort -t . -k2r,5 | grep demo | head -1)
+PAN_DL_URL=$(wget --header="Accept: text/html" --user-agent="$UA_STRING" -qO- https://support.wyze.com/hc/en-us/articles/360024852172-Release-Notes-Firmware | grep -Eo "(http|https)://[a-zA-Z0-9./?=_-]*" | grep /pan/ |   sort -t . -k2r,5 | grep demo | head -1)
 
 echo "checking for tools"
 command -v wget >/dev/null 2>&1 || { echo >&2 "wget is not installed.  Aborting."; exit 1; }
@@ -12,23 +13,29 @@ echo "create temporary work directory"
 mkdir v2_ro
 
 echo "check for local zip"
-if [ -f demo_4.9.8.1002.zip ]; then
+if [ -f demo.zip ]; then
 	echo "local archive found"
-	mv demo_4.9.8.1002.zip ./v2_ro/
+	mv demo.zip ./v2_ro/
 else
 	echo "local archive not found"
 	echo "downloading latest firmware"
-	wget $DL_URL -P ./v2_ro/
+	if [[ "$1" == "pan" ]]; then
+		echo "build for pan"
+		wget $PAN_DL_URL -O ./v2_ro/demo.zip
+	elif [[ "$1" == "" ]]; then
+		echo "build for v2"
+		wget $DL_URL -O ./v2_ro/demo.zip
+	fi
 fi
 
 echo "extracting firmware to temporary work directory"
-unzip v2_ro/demo_4.9.8.1002.zip -d ./v2_ro/
+unzip v2_ro/demo*.zip -d ./v2_ro/
 
 echo "create firmware directory"
 mkdir v2_ro/fw_dir
 
 echo "unpack stock firmware image"
-./fw_tool.sh unpack v2_ro/demo_4.9.8.1002.bin v2_ro/fw_dir
+./fw_tool.sh unpack v2_ro/demo*.bin v2_ro/fw_dir
 
 echo "replace factory kernel"
 cp v2_kernel.bin v2_ro/fw_dir/kernel.bin
