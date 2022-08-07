828ca2a5e5926d00caf8d1c7bde3f2c68314a735
Alfonso Gamboa <gtxent@gmail.com>
2022-08-06 23:05:20 -0700

    add netmon for bonding

--- SD_ROOT/wz_mini/usr/bin/netmon.sh
+++ SD_ROOT/wz_mini/usr/bin/netmon.sh
@@ -3,8 +3,12 @@
 . /opt/wz_mini/wz_mini.conf
 . /opt/wz_mini/etc/rc.common
 
-
 if [[ "$ENABLE_USB_DIRECT" == "true" ]]; then
-        wait_for_wlan_ip $(basename "$0")
-        gateway_supervisor $(basename "$0")
+	wait_for_wlan_ip $(basename "$0")
+	sleep 5
+	gateway_supervisor $(basename "$0") &
 fi
+
+echo "kill udhcpc extra"
+kill $(pgrep -f 'udhcpc -i wlan0 -H WyzeCam')
+
